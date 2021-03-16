9fcd85c90ec09205110897f422d62ba9f230a8a7
0qy <linqianye333@gmail.com>
2021-03-16 20:13:12 +1300

    minor changes

--- backend/config/env/production/database.js
+++ backend/config/env/production/database.js
@@ -1,5 +1,6 @@
 const parse = require('pg-connection-string').parse;
-const config = parse(process.env.'postgres://paxyxfqiajrjbz:88dfdd5113b026dc663d153e5dda80b6093b5aad84b359df1b41096b8ba80799@ec2-34-252-251-16.eu-west-1.compute.amazonaws.com:5432/d22k89v4ebavmi');
+const config = parse(process.env.DATABASE_URL);
+
 
 module.exports = ({ env }) => ({
     defaultConnections: 'default',
