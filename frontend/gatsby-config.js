a04495f9fc7efc2141f369592291a04cb0c548f1
0qy <linqianye333@gmail.com>
2021-03-19 12:34:33 +1300

    gatsby config

--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Qianye Lin Frontend`,
+    description: `front end of QYL`,
+    author: `Qianye Lin`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
