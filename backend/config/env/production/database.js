149bb5941fca391a5f9fbe48f83e5e8a9d298b59
0qy <linqianye333@gmail.com>
2021-03-19 13:46:20 +1300

    production database.js config (unfinished)

--- backend/config/env/production/database.js
+++ backend/config/env/production/database.js
@@ -1,26 +1,17 @@
-const parse = require('pg-connection-string').parse;
-const config = parse(process.env.DATABASE_URL);
-
-
-module.exports = ({ env }) => ({
-    defaultConnections: 'default',
-    connections: {
-        default: {
-            connector: 'bookshelf',
-            settings: {
-                client: 'postgres',
-                host: config.host,
-                port: config.port,
-                database: config.database,
-                username: config.user,
-                password: config.password,
-                ssl: {
-                    rejectUnauthorised: false,
-                },
+{
+    "defaultConnections": 'default',
+    "connections": {
+        "default": {
+            "connector": 'strapi-hook-bookshelf',
+            "settings": {
+                "client": 'postgres',
+                "host": "${process.env.DATABASE_HOST}",
+                "port": "${process.env.DATABASE_PORT}",
+                "database": "${process.env.DATABASE_NAME}",
+                "username": "${process.env.DATABASE_USERNAME}",
+                "password": "${process.env.DATABASE_PASSWORD}",
+                "ssl": true
             },
-            options: {
-                ssl: true,
-            },
-        },
-    },
-});
+            "options": {}
+
+}
